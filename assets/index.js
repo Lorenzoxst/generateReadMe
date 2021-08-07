5ee47c2cdc5a85d5befa610af42e9c9ba15d6f33
renzoxst <lorenzoxst@gmail.com>
2021-08-07 17:23:09 -0500

    General Structure completed

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,80 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+const { type } = require("os");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+    {
+        message: "What Would you like to name the README file?",
+        type: "input",
+        name: "title"
+    },
+    {
+        message: "Please provide a description for your project.",
+        type: "editor",
+        name: "description"
+    },
+    {
+        message: "Would you like to include a Table of contents?",
+        type: "confirm",
+        name: "toc",
+    },
+    {
+        message: "How would you like users to install your application?",
+        type: "input",
+        name: "install"
+    },
+    {
+        message: "Please include your usage information",
+        type: "input",
+        name: "usage"
+    },
+    {
+        message: "What license is this project using?",
+        type: "list",
+        choices: ["Apache License 2.0", "GNU GPLv3", "MIT License", "The Unlicense"],
+        name: "license"
+    },
+    {
+        message: "What are your contribution guidelines?",
+        type: "input",
+        name: "contribution"
+    },
+    {
+        message: "What are your test instructions?",
+        type: "input",
+        name: "test"
+    },
+    {
+        message: "What is your GitHub username?",
+        type: "input",
+        name: "github"
+    },
+    {
+        message: "What is the email address you would like to include for inquiries?",
+        type: "input",
+        name: "email"
+    },
+]);
+};
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data) )
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+        .then((data) => fs.writeFile("README.md", generateMarkdown(data) ))
+        .then(() => console.log('Successfully wrote and generated readme'))
+        .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -12,9 +12,40 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  let includeTOC = `${data.toc}`
+  let toc;
+if (includeTOC === true) {
+    return toc = `
+    ## Table of contents
+    [Installation](#installation)
+    [Usage](#usage)
+    [License](#license)
+    [Contributing](#contributing)
+    [Tests](#tests)
+    [Questions](#questions)
+    `
+}
+  
   return `# ${data.title}
 
-`;
+    ## Description
+    ${data.description}
+    ${toc}
+    ## Installation 
+    ${data.install}
+    ## Usage
+    ${data.usage}
+    ## License 
+    ${data.license}
+    ## Contributing 
+    ${data.contribution}
+    ## Tests
+    ${data.test}
+    ## Questions
+    [Link to my GitHub] (https://github.com/${data.github}/)
+    For any questions, you can reach me at my email: ${data.email}.
+`
+;
 }
 
 module.exports = generateMarkdown;
